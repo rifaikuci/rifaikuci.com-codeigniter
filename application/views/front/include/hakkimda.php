c5e2d98c4baba28e9b0f05fea9ff3a081ee29799
Rifai Kuçi <rfai4747@hotmail.com>
2019-12-11 21:29:34 +0300

    hakkımda anasayfaki beceriler kısmı kaldırıldı, bilgiler resmin altına getirilme işlemleri yapıldı.

--- application/views/front/include/hakkimda.php
+++ application/views/front/include/hakkimda.php
@@ -14,19 +14,44 @@
 
                     foreach ($siteayar as  $siteayar) {
                     ?>
-                    <img width="150" height="150" alt="<?php echo $siteayar['adsoyad'];?>"  src="<?php echo $siteayar['resim'];?>" class="img-fluid rounded b-shadow-a" alt="">
+                    <img  alt="<?php echo $siteayar['adsoyad'];?>"  src="<?php echo $siteayar['resim'];?>" class="img-fluid rounded b-shadow-a" >
                   </div>
                 </div>
                 <div class="col-sm-6 col-md-7">
-                  <div class="about-info">
-                    <p><span class="title-s">Ad Soyad: </span> <span><?php echo $siteayar['adsoyad'];?></span></p>
-                    <p><span class="title-s">Bölüm: </span> <span><?php echo $siteayar['ozellik'];?></span></p>
-                  <p><span class="title-s">Email: </span>  <a href="mailto:rfai0747@gmail.com?Subject=rifaikuci.com%20Adresinden" target="_top"> <span><?php echo $siteayar['mail'];?></span></a></p>
-                    
-                  </div>
+
                 </div>
               </div>
-            <!--<?php $this->load->view('front/include/ozellik.php'); ?> -->
+                <div>
+                <h1 class="title-left">
+                        Bilgilerim
+                </h1>
+                </div>
+                <br>
+
+                <p><span class="title-s">Ad Soyad: </span> <span><?php echo $siteayar['adsoyad'];?></span></p>
+                <p><span class="title-s">Bölüm: </span> <span><?php echo $siteayar['ozellik'];?></span></p>
+                <p><span class="title-s">Email: </span>  <a href="mailto:rfai0747@gmail.com?Subject=rifaikuci.com%20Adresinden" target="_top"> <span><?php echo $siteayar['mail'];?></span></a></p>
+                <div class="socials">
+                    <h2 >Sosyal Medya Hesapları</h2>
+                    <hr>
+                    <ul>
+                        <?php $smedya=smedyaion();
+                        foreach ($smedya as $smedya) {
+                            ?>
+                            <li  style="margin-top:20px">
+                                <a target="_blank" href="<?php echo $smedya ['url']; ?>">
+                                    <span class="ico-circle"><i class="ion-social-<?php echo $smedya['seoAd']; ?>"></i></span></a></li>
+                        <?php } ?>
+
+                        <li  style="margin-top:20px">
+                            <a target="_blank" href="mailto:rfai0747@gmail.com?Subject=rifaikuci.com%20Adresinden">
+                                <span class="ico-circle"><i class="ion-email"></i></span></a></li>
+                    </ul>
+                </div>
+                    <!-- özellik kısmı pasif edildi
+                     nedeni belirlediğim seviye etik değil, puanlama neye göre yapıldı.
+                     -->
+                <!--<?php $this->load->view('front/include/ozellik.php'); ?> -->
             </div>
             <div class="col-md-6">
               <div class="about-me pt-4 pt-md-0">
